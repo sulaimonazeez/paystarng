6304fa431afe999ea857b4874905bb467fb66787
sulaimonazeez <olaniyisulaimon221@gmail.com>
2026-01-01 16:39:28 -0600

    committed

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -4,6 +4,9 @@ import axios from "axios";
 export const AuthContext = createContext();
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
+
+
+//back to comment later huh 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
